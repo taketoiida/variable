b8f9fd4da0e91cd9f9307b7152577eef8e0cc6bf
taketoiida <taketo.iida.e379@dirbato.co.jp>
2024-04-05 14:35:09 +0900

    Calc_1435

---
+++ variable/src/sample/Calc.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class Calc {
+
+    public static void main(String[] args) {
+        var num1 = 100;
+        var num2 = 10;
+        
+        var result1 = num1 + num2;
+        System.out.println("num1 + num2 = "+result1);
+        
+        var result2 = num1 - num2;
+        System.out.println("num1 - num2 = " + result2);
+        
+        var result3 = num1 * num2;
+        System.out.println("num1 * num2 = " + result3);
+        
+        var result4 = num1 / num2;
+        System.out.println("num1 / num2 = " + result4);
+        
+        var result5 = num1 % num2;
+        System.out.println("num1 % num2 = " + result5);
+        // TODO 自動生成されたメソッド・スタブ
+
+    }
+
+}
